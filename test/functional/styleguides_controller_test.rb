3bf7b6ec2eeda097fe18a91c8a4e6bf709755a3b
Chad Pytel <cpytel@thoughtbot.com>
2009-07-24 16:44:48 -0400

    got tests to pass by properly loading views

--- test/functional/styleguides_controller_test.rb
+++ test/functional/styleguides_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ApplicationController < ActionController::Base
 end
 
+ActionController::Base.prepend_view_path File.join(File.dirname(__FILE__), '..', '..', 'app', 'views')
 require "flutie/styleguides_controller"
 require File.join(File.dirname(__FILE__), '..', '..', 'config', 'flutie_routes')
 
