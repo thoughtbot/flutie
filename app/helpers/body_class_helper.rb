53d093b4a6f003d0d039a80c617fe2dd1242727b
Matt Jankowski <mjankowski@thoughtbot.com>
2011-09-02 14:12:40 -0400

    add body class helper

---
+++ app/helpers/body_class_helper.rb
@@ -0,0 +1,6 @@
+module BodyClassHelper
+  def body_class
+    qualified_controller_name = controller.controller_path.gsub('/','-')
+    "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name}"
+  end
+end
