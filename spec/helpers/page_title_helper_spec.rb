b978d6ae098726f426db431d912ad693f8dd1254
Matt Jankowski <mjankowski@thoughtbot.com>
2011-09-04 15:22:29 -0400

    stay with 1.8 style hashes so that tests can run outside 1.9

--- spec/helpers/page_title_helper_spec.rb
+++ spec/helpers/page_title_helper_spec.rb
@@ -6,7 +6,7 @@ describe PageTitleHelper do
     it { should == "Dummy" }
   end
   describe "#page_title with site name" do
-    subject { helper.page_title(app_name: 'Test') }
+    subject { helper.page_title(:app_name => 'Test') }
     it { should == "Test" }
   end
   describe "#page_title with content for page title" do
@@ -16,12 +16,12 @@ describe PageTitleHelper do
   end
   describe "#page_title with content for page title and alternate separator" do
     before { helper.content_for(:page_title, 'page title') }
-    subject { helper.page_title(separator: ' | ') }
+    subject { helper.page_title(:separator => ' | ') }
     it { should == "Dummy | page title" }
   end
   describe "#page_title with content for alternate symbol" do
     before { helper.content_for(:alt_page_title, 'page title') }
-    subject { helper.page_title(page_title_symbol: :alt_page_title) }
+    subject { helper.page_title(:page_title_symbol => :alt_page_title) }
     it { should == "Dummy : page title" }
   end
 end
