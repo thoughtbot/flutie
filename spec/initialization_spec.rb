92718a0fa18ff4c1333c157ec3ebbaaf3d8ac7e8
Matt Jankowski <mjankowski@thoughtbot.com>
2011-09-04 17:56:09 -0400

    rails 3.1 stores the stylesheet expansions elsewhere

--- spec/initialization_spec.rb
+++ spec/initialization_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 describe "initialize the rails app" do
-  it "should add flutie directory to stylesheet expansion" do
-    expected = { :flutie => ['/flutie/stylesheets/flutie'] }
-    ActionView::Helpers::AssetTagHelper.send(:class_variable_get, :@@stylesheet_expansions).should == expected
+  let(:expected) { { :flutie => ['/flutie/stylesheets/flutie'] } }
+  if defined?(ActionView::Helpers::AssetTagHelper::StylesheetIncludeTag)
+    it "should add flutie directory to the stylesheet include tag expansions" do
+      ActionView::Helpers::AssetTagHelper::StylesheetIncludeTag.expansions.should == expected
+    end
+  else
+    it "should add flutie directory to stylesheet expansion" do
+      expected = { :flutie => ['/flutie/stylesheets/flutie'] }
+      ActionView::Helpers::AssetTagHelper.send(:class_variable_get, :@@stylesheet_expansions).should == expected
+    end
   end
 end
+
