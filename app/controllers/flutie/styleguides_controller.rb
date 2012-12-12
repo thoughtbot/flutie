6b9bf1fb23d1d943b0c47a6416f7290a9113b52c
Matt Jankowski <mjankowski@thoughtbot.com>
2012-12-12 14:07:55 -0500

    Providing the full file name including the extension causes a deprecation warning - remove the extension

--- app/controllers/flutie/styleguides_controller.rb
+++ app/controllers/flutie/styleguides_controller.rb
@@ -15,7 +15,7 @@ module Flutie
       all_styleguide_partials = ActionController::Base.view_paths.map do |view_path|
         styleguide_path = File.join(view_path.to_s, 'styleguides')
         if File.exists?(styleguide_path)
-          Dir.glob(styleguide_path + '/_*.*')
+          Dir.glob(styleguide_path + '/_*.*').map { |file| file.chomp(File.extname(file)) }
         else
           nil
         end
