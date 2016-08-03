9c66341dd5481baa85b83b275d6c4940639a1649
zwelchcb <Zachary.Welch@careerbuilder.com>
2016-08-03 18:26:28 -0400

    Refactor body_class and page_title using compact

--- lib/flutie/body_class_helper.rb
+++ lib/flutie/body_class_helper.rb
@@ -4,10 +4,6 @@ module BodyClassHelper
     qualified_controller_name = controller.controller_path.gsub('/','-')
     basic_body_class = "#{qualified_controller_name} #{qualified_controller_name}-#{controller.action_name}"
 
-    if content_for?(extra_body_classes_symbol)
-      [basic_body_class, content_for(extra_body_classes_symbol)].join(' ')
-    else
-      basic_body_class
-    end
+    [basic_body_class, content_for(extra_body_classes_symbol)].compact.join(' ')
   end
 end
