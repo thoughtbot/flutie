1561a7924022458ecce3a01f3ccf7118adc8e044
Matt Jankowski <mjankowski@thoughtbot.com>
2018-09-27 14:27:09 -0400

    Style update on specs

--- spec/helpers/body_class_helper_spec.rb
+++ spec/helpers/body_class_helper_spec.rb
@@ -1,47 +1,47 @@
-require "rails_helper"
+require 'rails_helper'
 
 describe BodyClassHelper, type: :helper do
-  describe "a normal controller" do
+  describe 'a normal controller' do
     before do
-      allow(helper).to receive(:controller).
-        and_return(controller_double)
+      allow(helper).to receive(:controller)
+        .and_return(controller_double)
     end
     subject { helper.body_class }
-    it { should == "widgets widgets-show" }
+    it { should == 'widgets widgets-show' }
   end
 
-  describe "a nested controller" do
+  describe 'a nested controller' do
     before do
-      allow(helper).to receive(:controller).
-        and_return(controller_double("modules/widgets"))
+      allow(helper).to receive(:controller)
+        .and_return(controller_double('modules/widgets'))
     end
     subject { helper.body_class }
-    it { should == "modules-widgets modules-widgets-show" }
+    it { should == 'modules-widgets modules-widgets-show' }
   end
 
-  describe "extra body classes" do
+  describe 'extra body classes' do
     before do
-      allow(helper).to receive(:controller).
-        and_return(controller_double)
-      helper.content_for(:extra_body_classes, "extra_class")
+      allow(helper).to receive(:controller)
+        .and_return(controller_double)
+      helper.content_for(:extra_body_classes, 'extra_class')
     end
     subject { helper.body_class }
-    it { should == "widgets widgets-show extra_class" }
+    it { should == 'widgets widgets-show extra_class' }
 
-    describe "multiples times" do
+    describe 'multiples times' do
       before do
-        helper.content_for(:extra_body_classes, " many extra classes")
+        helper.content_for(:extra_body_classes, ' many extra classes')
       end
       subject { helper.body_class }
-      it { should == "widgets widgets-show extra_class many extra classes" }
+      it { should == 'widgets widgets-show extra_class many extra classes' }
     end
   end
 
-  def controller_double(path = "widgets")
+  def controller_double(path = 'widgets')
     double(
       :controller,
       controller_path: path,
-      action_name: "show",
+      action_name: 'show'
     )
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,14 @@
 # Configure Rails Envinronment
-ENV["RAILS_ENV"] = "test"
+ENV['RAILS_ENV'] = 'test'
 
-require "spec_helper"
+require 'spec_helper'
 
-require "action_controller/railtie"
-require "fake_app"
+require 'action_controller/railtie'
+require 'fake_app'
 Flutie::Application.initialize!
 
-require "flutie/railtie"
-require "rspec/rails"
+require 'flutie/railtie'
+require 'rspec/rails'
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
